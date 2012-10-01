ebb24e15ee42f52d41430cf6ed839453c7fd8ba2
Xavier Rubio <xrubio@HAL9012.(none)>
2012-10-01 15:25:37 +0200

    + september simulpast release

--- pandora/examples/gujarat/explorationMN/exploration_local.py
+++ pandora/examples/gujarat/explorationMN/exploration_local.py
@@ -4,9 +4,12 @@ import fileinput, sys, os, random
 #mapSizes = ['400','800','1600']
 mapSizes = ['1600']
 
-numExecutions = 10
-numHGs = ['1']
+numExecutions = 2
+numHGs = ['100']
 controllers = ['DecisionTree']
+#controllers = ['DecisionTree', 'MDP', 'Random']
+#biomassDistributions = ['standard', 'logDecayFromWater', 'linDecayFromWater']
+biomassDistributions = ['linDecayFromWater']
 
 xmlTemplate = 'templates/config_template_local.xml'
 
@@ -19,6 +22,7 @@ initialDirKey = 'INITIALDIR'
 numExecutionKey = 'NUMEXEC'
 
 climateKey = 'CLIMATESEED'
+biomassDistributionKey = 'BIOMASS_DISTRIBUTION'
 
 
 def replaceKey( fileName, key, value ):
@@ -41,27 +45,30 @@ for numExecution in range(0,numExecutions):
 	for mapSize in mapSizes:
 		for numHG in numHGs:
 			for controller in controllers:
-				print 'creating gujarat instance: ' + str(index) + ' for map: ' + mapSize + ' numHG: ' + numHG + ' with controller: ' + controller + ' and execution: ' + str(numExecution)
-				dirName = 'results_size'+mapSize+'_numHG'+numHG+'_controller_'+controller+'_ex'+str(numExecution)
-				os.system('mkdir '+dirName)
-				configName = dirName + '/config.xml'			
-				os.system('cp '+xmlTemplate+' '+configName)
-				replaceKey(configName, mapSizeKey, mapSize)
-				replaceKey(configName, numHGKey, numHG)
-				replaceKey(configName, controllerKey, controller)
-				replaceKey(configName, climateKey, randomValue)
-				replaceKey(configName, numExecutionKey, str(numExecution))
+				for biomassDistribution in biomassDistributions:
+					print 'creating gujarat instance: ' + str(index) + ' for map: ' + mapSize + ' numHG: ' + numHG + ' with controller: ' + controller + ' biomass distribution: ' + biomassDistribution + ' and execution: ' + str(numExecution)
+					dirName = 'results_size'+mapSize+'_numHG'+numHG+'_controller_'+controller+'_biodist_'+biomassDistribution+'_ex'+str(numExecution)
+					os.system('mkdir '+dirName)
+					configName = dirName + '/config.xml'			
+					os.system('cp '+xmlTemplate+' '+configName)
+					replaceKey(configName, mapSizeKey, mapSize)
+					replaceKey(configName, biomassDistributionKey, biomassDistribution)
+					replaceKey(configName, numHGKey, numHG)
+					replaceKey(configName, controllerKey, controller)
+					replaceKey(configName, climateKey, randomValue)
+					replaceKey(configName, numExecutionKey, str(numExecution))
 
-				index += 1
+					index += 1
 
 print 'workbench done, submitting tasks'
 index = 0
 for mapSize in mapSizes:
 	for numHG in numHGs:
-		for controller in controllers:	
-			for numExecution in range(0,numExecutions):
-				print 'submitting gujarat instance: ' + str(index) + ' for map: ' + mapSize + ' numHG: ' + numHG + ' with controller: ' + controller + ' and execution: ' + str(numExecution)
-				dirName = 'results_size'+mapSize+'_numHG'+numHG+'_controller_'+controller+'_ex'+str(numExecution)
-				os.system('../gujarat '+dirName+'/config.xml >'+dirName+'/gujarat.log 2>'+dirName+'/gujarat.err &')
-				index += 1
+		for controller in controllers:
+			for biomassDistribution in biomassDistributions:
+				for numExecution in range(0,numExecutions):
+					print 'submitting gujarat instance: ' + str(index) + ' for map: ' + mapSize + ' numHG: ' + numHG + ' with controller: ' + controller + ' and execution: ' + str(numExecution)
+					dirName = 'results_size'+mapSize+'_numHG'+numHG+'_controller_'+controller+'_biodist_'+biomassDistribution+'_ex'+str(numExecution)
+					os.system('../gujarat '+dirName+'/config.xml >'+dirName+'/gujarat.log 2>'+dirName+'/gujarat.err &')
+					index += 1
 
