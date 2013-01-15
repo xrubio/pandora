c2244718f6e1cdbd20a5c21d58b9569ab9c8c431
atorrano <atorrano@bsc.es>
2013-01-15 14:36:48 +0100

    minor bug

--- pandora/examples/gujarat/Climate.cxx
+++ pandora/examples/gujarat/Climate.cxx
@@ -1,5 +1,4 @@
 
-
 #include <Climate.hxx>
 #include <GujaratConfig.hxx>
 #include <GujaratWorld.hxx>
@@ -14,9 +13,11 @@ Climate::Climate( const GujaratConfig & config, const GujaratWorld& theWorld )
 	: _randomGenerator((long int)config._climateSeed), _currentSeason(HOTDRY),  
 	_currentRain(0.0f),
 	_uniformDistribution(_randomGenerator, boost::uniform_real <> (0,1)), 
-	_alphaRain ( 	(_config._rainHistoricalDistribMean/_config._rainHistoricalDistribStdev)
-	*(_config._rainHistoricalDistribMean/_config._rainHistoricalDistribStdev)  ),
-	_betaRain ( (_config._rainHistoricalDistribStdev*_config._rainHistoricalDistribStdev)/_config._rainHistoricalDistribMean ),
+	_alphaRain ( 	(config._rainHistoricalDistribMean/config._rainHistoricalDistribStdev)
+	*(config._rainHistoricalDistribMean/config._rainHistoricalDistribStdev)  ),
+	_betaRain ( config._rainHistoricalDistribMean
+				/
+			(config._rainHistoricalDistribStdev*config._rainHistoricalDistribStdev) ),
 	_config(config), _theWorld( theWorld )
 {
 }
@@ -42,14 +43,11 @@ void Climate::step()
 	if(_currentSeason == HOTWET )
 	{
 		// TODO document ODD : change weibull for gamma
-		//std::gamma_distribution<float> distributionRain(_alphaRain,_betaRain);
 		
 		boost::gamma_distribution<> gd(_alphaRain);
-		
 		boost::variate_generator<boost::mt19937&, boost::gamma_distribution<> >
 		var_gamma(_randomGenerator, gd);
 		
-		//_currentRain = distributionRain(_randomGenerator);
 		_currentRain = var_gamma()*1.0/_betaRain;
 	}
 }
