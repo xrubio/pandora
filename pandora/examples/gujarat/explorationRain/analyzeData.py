1c43ffe41379bd4c343140b840196c3f41cc3041
Xavier Rubio <xrubio@myubero-laptop.(none)>
2013-01-20 16:36:30 +0100

    + exploration of rain mean and stddev

--- pandora/examples/gujarat/explorationRain/analyzeData.py
+++ pandora/examples/gujarat/explorationRain/analyzeData.py
@@ -3,11 +3,14 @@
 import os, sys
 
 outputDir = 'finalResults'
-dirBase = '/gpfs/projects/bsc21/WORK-RUBIO/datalogs_ex1/'
+dirBase = '/gpfs/projects/bsc21/WORK-RUBIO/'
 resultsBase = '.'
 
 numExecutions = 1
-minimumBiomassValues = ['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0']
+minimumBiomassValues = ['0.1']
+meanValues = ['4643']
+stdDevValues = ['1885']
+
 logsDir = outputDir+'/logs'
 
 os.system('rm -rf '+outputDir)
@@ -17,13 +20,14 @@ os.makedirs(logsDir)
 index = 0
 for numExecution in range(0,numExecutions):
 	for minimumBiomass in minimumBiomassValues:
-			print 'analyzing results for instance with: ' + str(index) + ' with min biomass: ' + minimumBiomass + ' and execution: ' + str(numExecution)
-			fileName = dirBase + 'data_biomin'+minimumBiomass+'_ex'+str(numExecution)+'/gujarat.h5'
-			outputAgents = outputDir+'/agents_biomin'+minimumBiomass+'_ex'+str(numExecution)+'.csv'
-			outputRasters = outputDir+'/rasters_biomin'+minimumBiomass+'_ex'+str(numExecution)+'.csv'
-			outputLogDir = dirBase+'/logs_biomin'+minimumBiomass+'_ex'+str(numExecution)
+			print 'analyzing results for instance with: ' + str(index) + ' with min biomass: ' + minimumBiomass + ' mean: ' + mean + ' stddev: ' + stddev + ' and execution: ' + str(numExecution)
+			fileName = dirBase + 'data_biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution)+'/gujarat.h5'
+			outputAgents = outputDir+'/agents_biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution)+'.csv'
+			outputRasters = outputDir+'/rasters_biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution)+'.csv'
+			outputLogDir = dirBase+'/logs_biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution)
+			outputResultsDir = resultsBase+'/results_biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution)
 			os.system('../analysis/analysis '+fileName+' ' + outputAgents + ' ' + outputRasters)
-			os.system('mkdir '+logsDir+'/biomin_'+minimumBiomass+'_ex'+str(numExecution))
-			os.system('cp '+outputLogDir+'/* '+logsDir+'/biomin_'+minimumBiomass+'_ex'+str(numExecution))
-#			os.system('cp '+resultsBase+'/* '+logsDir+'/biomin_'+minimumBiomass+'_ex'+str(numExecution))
+			os.system('mkdir '+logsDir+'/biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution))
+			os.system('cp '+outputLogDir+'/* '+logsDir+'/biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution))
+			os.system('cp '+outputResultsDir+'/* '+logsDir+'/biomin'+minimumBiomass+'_mean'+mean+'_stddev'+stddev+'_ex'+str(numExecution))
 
