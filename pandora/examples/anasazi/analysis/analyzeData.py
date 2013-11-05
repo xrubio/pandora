563efbe277b70ab63cbfdf19f2e74dc9a451d379
Xavier Rubio <xavier.rubio@bsc.es>
2013-11-05 10:59:45 +0000

    + Virtual anasazi model

---
+++ pandora/examples/anasazi/analysis/analyzeData.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys, random
+sys.path.append('/home/xrubio/workspace/pandora/pandora/pyPandora/')
+sys.path.append('/home/xrubio/workspace/pandora/pandora/')
+from pyPandora import Simulation, Agent, World, Point2DInt, SimulationRecord, GlobalAgentStats, AgentNum, AgentMean, AgentSum, AgentStdDev, GlobalRasterStats, RasterMean, RasterSum
+
+record = SimulationRecord(1, False)
+record.loadHDF5('../data/anazasi.h5', True, True)
+
+agentResults = GlobalAgentStats(';')
+agentResults.addAnalysis(AgentNum())
+#agentResults.addAnalysis(AgentMean('estimates'))
+#agentResults.addAnalysis(AgentStdDev('strength x100'))
+#agentResults.addAnalysis(AgentSum('strength x100'))
+agentResults.applyTo(record,'households.csv', 'Household')
+
+#rasterResults = GlobalRasterStats(';')
+#rasterResults.addAnalysis(RasterMean())
+#rasterResults.addAnalysis(RasterSum())
+
+#rasterResults.applyTo(record, 'water.csv', 'yield x100')
+
