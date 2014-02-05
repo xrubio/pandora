205e76caa8f4ca75ae9e11c8aba001146966dcac
Xavier Rubio <xrubio@HAL9006.(none)>
2014-02-05 13:40:34 +0100

    + updated to new analysis interface

--- docs/tutorials/04_src/tutorial_pyAnalysis.py
+++ docs/tutorials/04_src/tutorial_pyAnalysis.py
@@ -1,26 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 sys.path.append('../../../pandora/pyPandora/')
 sys.path.append('../../../pandora/')
-from pyPandora import SimulationRecord, AgentResults, AgentMean, AgentSum, AgentNum, AgentHDFtoSHP, RasterMean, RasterSum, RasterResults
+from pyPandora import SimulationRecord, GlobalAgentStats, AgentMean, AgentSum, AgentNum, RasterMean, RasterSum, GlobalRasterStats
 
 record = SimulationRecord()
-record.loadHDF5('data/results.h5', 1, 1)
+record.loadHDF5('data/results.h5', True, True)
 
-agentResults = AgentResults(record, 'agents.csv', 'MyAgent', ';')
+agentResults = GlobalAgentStats(';')
 agentResults.addAnalysis(AgentNum())
 agentResults.addAnalysis(AgentMean('x'))
 agentResults.addAnalysis(AgentMean('y'))
 agentResults.addAnalysis(AgentMean('resources'))
 agentResults.addAnalysis(AgentSum('resources'))
-agentResults.addAnalysis(AgentHDFtoSHP('shp/agents.shp', -1))
 
-agentResults.apply()
+agentResults.applyTo(record, 'agents.csv', 'MyAgent')
 
-rasterResults = RasterResults(record, 'rasters.csv', 'resources', ';')
+rasterResults = GlobalRasterStats(';')
 rasterResults.addAnalysis(RasterMean())
 rasterResults.addAnalysis(RasterSum())
-
-rasterResults.apply()
+rasterResults.applyTo(record, 'rasters.csv', 'resources')
 
