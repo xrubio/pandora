44de680503377a58f249c136c65195835115c293
Xavier Rubio <xrubio@HAL9006.(none)>
2014-02-27 11:49:37 +0100

    + added test for removing agents

--- pandora/pyPandora/tests/tests.py
+++ pandora/pyPandora/tests/tests.py
@@ -9,11 +9,31 @@ sys.path.append('../../')
 
 from pyPandora import Simulation, Agent, World, Point2DInt
 
+class TestAgent(Agent):
+	def __init__(self, id):
+		Agent.__init__( self, id)
+
+	def updateState(self):
+		print(self,'executed')
+		return
+
+	def serialize(self):
+		return
+
+class TestWorld(World):
+	def __init__(self, simulation ):
+		World.__init__( self, simulation)
+
+	def createRasters(self):
+		return
+
+	def createAgents(self):
+		return
+
 class TestPyPandora(unittest.TestCase):
 	
 	def setUp(self):
 		return
-	
 	
 	def testEqualityPoint(self):
 		point1 = Point2DInt(4,5)
@@ -34,6 +54,19 @@ class TestPyPandora(unittest.TestCase):
 		self.assertNotEqual(mySimulation.size, size)
 		size._x = 10
 		self.assertEqual(mySimulation.size, size)
+
+	def testAgentRemovedIsNotExecuted(self):
+		mySimulation = Simulation(Point2DInt(10,10), 1)
+		myWorld = TestWorld(mySimulation)
+		myWorld.initialize()
+
+		myAgent = TestAgent('agent_0')
+		myWorld.addAgent(myAgent)
+		myAgent.setRandomPosition()
+		self.assertEqual(myAgent.exists, True)
+		myAgent.remove()
+		self.assertEqual(myAgent.exists, False)
+		myWorld.run()
        
 if __name__ == '__main__':
     unittest.main()
